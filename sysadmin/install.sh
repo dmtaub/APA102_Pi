951e0e495a7f20b61d48c44d416d0070f28c91ba
mDan <dmtaub@gmail.com>
2015-09-29 23:45:40 +0000

    fixing pcmanfm command and crontab to update background

--- sysadmin/install.sh
+++ sysadmin/install.sh
@@ -3,5 +3,7 @@
 sudo cp runlights /etc/init.d/
 sudo update-rc.d runlights defaults
 sudo apt-get install imagemagick feh
+( crontab -l 2>/dev/null | grep -Fv ipbg ; printf -- "*/3 * * * * /home/pi/APA102_Pi/sysadmin/ipbg.sh" ) | crontab
+
 # uninstall
 #sudo update-rc.d -f runlights remove
--- sysadmin/ipbg.sh
+++ sysadmin/ipbg.sh
@@ -1,20 +1,34 @@
 #!/bin/bash  
 export DISPLAY=:0.0
 # Create white background image
-convert -size 1280x800 xc:white /tmp/base.jpg
+convert -size 640x400 xc:white /tmp/base.jpg
 
+POINT=50
+TOP=30
+LINE1H=`expr $POINT \* 3 + $TOP`
+LINE2H=`expr $POINT \* 4 + $TOP`
+LINE3H=`expr $POINT \* 6 + $TOP`
+DATE=`date +"%a %b %d %Y %H:%M:%S"`
 # Create IP image
-convert /tmp/base.jpg -pointsize 80 -fill lime -draw "text 0,150 'IPv4: $(ip -4 a s eth0 | grep -Eo 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk '{print $2}')'" -fill black -draw "text -0,250 'Hostname: $(uname -n)'" -pointsize 60 -draw "text -0,500 'Date $(date)'" bg_ip.jpg
+convert /tmp/base.jpg -pointsize $POINT -fill lime -draw "text 0,$LINE1H 'IPv4: $(ip -4 a s eth0 | grep -Eo 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk '{print $2}')'" -fill black -draw "text -0,$LINE2H 'Hostname: $(uname -n)'" -pointsize $POINT -draw "text -0,$LINE3H '$DATE'" bg_ip.jpg
 
 #xsetroot -bitmap bg_ip.xbm
 #pcmanfm --set-wallpaper=bg_ip.jpg
+
 # Uncomment this one if you're not using gnome:  
-#feh --bg-scale ./ip.jpg  
+#killall feh
+#feh ./bg_ip.jpg  &
+
+pcmanfm --set-wallpaper /home/pi/APA102_Pi/sysadmin/bg_ip.jpg --wallpaper-mode=center
+
+#sudo ln -fs `pwd`/bg_ip.jpg /etc/alternatives/desktop-background
+
+xset s reset #unblank screen
+echo `date` --running >>log.txt
+
 # and place a # (hash) for the following rule:   
 #gconftool -t string -s /desktop/gnome/background/picture_filename ./ip.jpg  
 
-xset s reset
 #lrwxrwxrwx 1 root root 52 May  6 23:02 /etc/alternatives/desktop-background -> /usr/share/raspberrypi-artwork/raspberry-pi-logo.png
-#ln -fs `pwd`/bg_ip.jpg /etc/alternatives/desktop-background
 #ln -fs /usr/share/raspberrypi-artwork/raspberry-pi-logo.png /etc/alternatives/desktop-background
 
