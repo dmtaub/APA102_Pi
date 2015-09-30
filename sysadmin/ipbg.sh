635db6089e8fa715074246a5ce0f232aa61f989a
mDan <dmtaub@gmail.com>
2015-09-30 00:08:44 +0000

    removing log statement, adding complete path for final image

--- sysadmin/ipbg.sh
+++ sysadmin/ipbg.sh
@@ -9,8 +9,9 @@ LINE1H=`expr $POINT \* 3 + $TOP`
 LINE2H=`expr $POINT \* 4 + $TOP`
 LINE3H=`expr $POINT \* 6 + $TOP`
 DATE=`date +"%a %b %d %Y %H:%M:%S"`
+FINAL_IMG=/home/pi/bg_ip.jpg
 # Create IP image
-convert /tmp/base.jpg -pointsize $POINT -fill lime -draw "text 0,$LINE1H 'IPv4: $(ip -4 a s eth0 | grep -Eo 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk '{print $2}')'" -fill black -draw "text -0,$LINE2H 'Hostname: $(uname -n)'" -pointsize $POINT -draw "text -0,$LINE3H '$DATE'" bg_ip.jpg
+convert /tmp/base.jpg -pointsize $POINT -fill lime -draw "text 0,$LINE1H 'IPv4: $(ip -4 a s eth0 | grep -Eo 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk '{print $2}')'" -fill black -draw "text -0,$LINE2H '$(whoami)@$(uname -n)'" -pointsize $POINT -draw "text -0,$LINE3H '$DATE'" $FINAL_IMG
 
 #xsetroot -bitmap bg_ip.xbm
 #pcmanfm --set-wallpaper=bg_ip.jpg
@@ -19,12 +20,12 @@ convert /tmp/base.jpg -pointsize $POINT -fill lime -draw "text 0,$LINE1H 'IPv4: 
 #killall feh
 #feh ./bg_ip.jpg  &
 
-pcmanfm --set-wallpaper /home/pi/APA102_Pi/sysadmin/bg_ip.jpg --wallpaper-mode=center
+pcmanfm --set-wallpaper $FINAL_IMG --wallpaper-mode=center
 
 #sudo ln -fs `pwd`/bg_ip.jpg /etc/alternatives/desktop-background
 
 xset s reset #unblank screen
-echo `date` --running >>log.txt
+echo `date` --running
 
 # and place a # (hash) for the following rule:   
 #gconftool -t string -s /desktop/gnome/background/picture_filename ./ip.jpg  
