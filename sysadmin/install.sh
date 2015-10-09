06b5f25e51b3c0d634296377b95cd0c865a39b96
Daniel <dmtaub@gmail.com>
2015-10-08 20:02:31 -0400

    adding more dependencies

--- sysadmin/install.sh
+++ sysadmin/install.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install imagemagick feh python-dev python-pip
+sudo pip install spidev
+
+
 sudo cp runlights /etc/init.d/
 sudo update-rc.d runlights defaults
-sudo apt-get install imagemagick feh
+
 ( crontab -l 2>/dev/null | grep -Fv ipbg ; printf -- "*/3 * * * * /home/pi/APA102_Pi/sysadmin/ipbg.sh" ) | crontab
 
 # uninstall
