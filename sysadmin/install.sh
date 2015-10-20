b5179b57bc44c6b5260821e8fbdc366533138b34
dmt <dmtaub@gmail.com>
2015-10-19 23:33:58 -0400

    adding kinet support

--- sysadmin/install.sh
+++ sysadmin/install.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 sudo cp runlights /etc/init.d/
+cd ~
+git clone -b dev https://github.com/dmtaub/kinet
 sudo update-rc.d runlights defaults
 sudo apt-get install imagemagick feh
 ( crontab -l 2>/dev/null | grep -Fv ipbg ; printf -- "*/3 * * * * /home/pi/APA102_Pi/sysadmin/ipbg.sh" ) | crontab
 
 # uninstall
 #sudo update-rc.d -f runlights remove
+echo alias rgb=\'~/kinet/rgb.py\' >> ~/.bashrc
